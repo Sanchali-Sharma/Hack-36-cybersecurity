f306c3ebe7c7c3478a8e6ba56f700d3b3b099976
Sambhav-Sam <97016770+Sambhav-Sam@users.noreply.github.com>
2023-03-25 18:02:56 +0530

    Added News Section

---
+++ frontend/src/News.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewsComponent from './component/NewsComponent'
+import Home from './Home'
+
+const News = () => {
+  return (
+    <>
+    <Home />
+    <NewsComponent />
+    <NewsComponent />
+    </>
+  )
+}
+
+export default News
